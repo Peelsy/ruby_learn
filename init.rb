6fc0345fff19fbcd47a809165de0a5b71920942c
Kts <k.tsimbarenko@starladder.tv>
2019-09-10 17:39:38 +0300

    R and W to file

--- init.rb
+++ init.rb
@@ -5,40 +5,40 @@ require_relative "real"
 require_relative "virtual"
 require_relative "order"
 
-
-item1 = VirtualItem.new({:real_price => 101, :weight => 100, :name => "car"})
-item2 = RealItem.new({ :real_price => 105, :weight => 100, :name => "car"})
-item3 = RealItem.new({ :real_price => 105, :weight => 100, :name => "computer"})
+@items = []
+@items << VirtualItem.new({:real_price => 106, :weight => 100, :name => "car"})
+@items << RealItem.new({ :real_price => 105, :weight => 100, :name => "kettle"})
+@items << RealItem.new({ :real_price => 105, :weight => 100, :name => "computer"})
 
 
 # item.info{ |attr| puts attr}
-puts Item.discount
-
-cart = Cart.new
-
-cart.add_item(item1)
-cart.add_item(item2)
-
-puts cart.items.size
-
-cart.delete_invalid_items
-
 # puts Item.discount
 
-p cart.items
+# cart = Cart.new
 
-# p item1.respond_to?(:weight)
-# p item2.respond_to?(:weight)
+# cart.add_item(item1)
+# cart.add_item(item2)
 
-# puts item1.price_with_discount
-# puts item1.real_price
+# puts cart.items.size
 
-order = Order.new
-order.add_item(item1)
-order.add_item(item2)
-order.add_item(item3)
-order.remove_item
+# cart.delete_invalid_items
 
-puts order.items.size
+# # puts Item.discount
 
-puts order.count_valid_items
+# p cart.items
+
+# # p item1.respond_to?(:weight)
+# # p item2.respond_to?(:weight)
+
+# # puts item1.price_with_discount
+# # puts item1.real_price
+
+# order = Order.new
+# order.add_item(item1)
+# order.add_item(item2)
+# order.add_item(item3)
+# order.remove_item
+
+# puts order.items.size
+
+# puts order.count_valid_items
